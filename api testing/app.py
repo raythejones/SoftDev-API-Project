625a4dee4be21a2e45aa916c5c83150a45038dec
Max Korsun <mkorsun@stuy.edu>
2017-11-25 18:30:46 -0500

    added a youtube method

--- api testing/app.py
+++ api testing/app.py
@@ -3,36 +3,26 @@ import urllib2
 import json
 import requests
 
-HEADERS = {"X-API-Key":'72504fb40d484be59b89d903b9d4ed08'}
 app = Flask(__name__)
+
+
 
 @app.route('/', methods = ['GET','POST'])
 def root():
-    u = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=RvEhHA59fuin9WI0oK3RxK7Te31EcneKjEvC0VAp")
-
-    dict = json.loads(u.read())
-    url = dict['url']
-    exp = dict['explanation']
-    return render_template("index.html", link=url, desc=exp)
-
-@app.route('/armory', methods = ['GET','POST'])
-def armory():
-    if request.args.get('search')=='Submit':
-        r=(requests.get("http://www.bungie.net/Platform/Destiny2/Armory/Search/DestinyInventoryItemDefinition/" + request.args.get('lookup'), headers=HEADERS))
-    else:                         
-        r=requests.get("http://www.bungie.net/Platform/Destiny2/Armory/Search/DestinyInventoryItemDefinition/ /", headers=HEADERS);
+                          
+    r=requests.get("http://www.bungie.net/Platform/Destiny2/Armory/Search/DestinyInventoryItemDefinition/ /", headers=HEADERS);
     dict=r.json()#Automaticaly runs json.load() onto r, whish is json formatted text
-    items=dict['Response']['results']['results']
-    for each in items:
-        del each['entityType']
-        del each['hash']
-        del each['weight']
+    items=dict
+   # for each in items:
+     #   del each['entityType']
+     #   del each['hash']
+     #   del each['weight']
     dispop=[]
-    for each in items:
-        dispop.append(each['displayProperties'])
+   # for each in items:
+    #    dispop.append(each['displayProperties'])
   
 
-    return render_template("armory.html", list=dispop)
+    return render_template("armory.html", list=items)
 
 if __name__ == "__main__":
     app.debug = True
--- utils/api.py
+++ utils/api.py
@@ -7,7 +7,7 @@ with open('keys.csv') as csvfile:
          print(row['first_name'], row['last_name'])
 
 def searchWalmart(query):
-    r=(requests.get('http://api.walmartlabs.com/v1/search?apiKey='+keys['Walmart']+'&query='+query+'&numItems=25'))
+    r=requests.get('http://api.walmartlabs.com/v1/search?apiKey='+keys['Walmart']+'&query='+query+'&numItems=25')
     dict=r.json()
     items=dict['items']
     realitems=[]
@@ -15,4 +15,12 @@ def searchWalmart(query):
         if each['marketplace'] =='false':
             realitems.append(each)
     return realitems
-    
+
+def showYoutube(query):
+     r=requests.get('https://www.googleapis.com/youtube/v3/search?part=snippet&q='+query+'&type=video&videoCaption=closedCaption&key='+keys['Youtube'])
+     dict=r.json()
+     items=dict['items']
+     video=items[0]['videoId']
+     link='https://www.youtube.com/watch?v='+video
+     return link
+     
