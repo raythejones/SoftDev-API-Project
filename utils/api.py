95d88d21d258fb47b7405590872bb909dcedfb2d
Max Korsun <mkorsun@stuy.edu>
2017-11-27 14:37:24 -0500

    edited api functions

--- utils/api.py
+++ utils/api.py
@@ -12,11 +12,21 @@ def searchWalmart(query):
     items=dict['items']
     realitems=[]
     for each in items:
-        if each['marketplace'] =='false':
+        if each['marketplace'] =='true':
             realitems.append(each)
     return realitems
 
-def showYoutube(query):
+
+def productInfo(dict):
+    info={}
+    info['name'] = dict['name']
+    info['image']= dict['largeImage']
+    info['link'] = dict['productUrl']
+    info['desc']= dict['longDescription']
+    return info
+    
+
+def showYoutube(name):
      r=requests.get('https://www.googleapis.com/youtube/v3/search?part=snippet&q='+query+'&type=video&videoCaption=closedCaption&key='+keys['Youtube'])
      dict=r.json()
      items=dict['items']
